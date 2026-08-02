24f67347e8a048fea99c71dd6ad58a539b17738a
NaimShk07 <nayemuddinshaikh@gmail.com>
2026-08-02 16:28:42 +0530

    feat: restructure database module and update related configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './database/prisma/prisma.module';
 import configuration from './config/configuration';
 
 @Module({
